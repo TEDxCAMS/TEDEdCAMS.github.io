a332b636ec679ca021764c6860c6366fd9bf9f94
isaacaddis <isaacaddis21@gmail.com>
2016-11-04 23:13:42 -0700

    Major Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,7 +42,42 @@
 
 		// Scrolly.
 			$('.scrolly').scrolly();
-
+        //EZ Typed.js
+        $("#typed").typed({
+            strings: ["sharing", "imagining","learning"],
+            // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+            stringsElement: null,
+            // typing speed
+            typeSpeed: 0,
+            // time before typing starts
+            startDelay: 0,
+            // backspacing speed
+            backSpeed: 0,
+            // shuffle the strings
+            shuffle: false,
+            // time before backspacing
+            backDelay: 500,
+            // loop
+            loop: true,
+            // false = infinite
+            loopCount: false,
+            // show cursor
+            showCursor: true,
+            // character for cursor
+            cursorChar: "|",
+            // attribute to type (null == text)
+            attr: null,
+            // either html or text
+            contentType: 'html',
+            // call when done callback function
+            callback: function() {},
+            // starting callback function before each string
+            preStringTyped: function() {},
+            //callback for every typed string
+            onStringTyped: function() {},
+            // callback for reset
+            resetCallback: function() {}
+        });
 	});
 
 })(jQuery);
