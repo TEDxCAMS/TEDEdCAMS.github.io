654e04706d2928d591e4a23b24443342d6265f16
isaacaddis <isaacaddis21@gmail.com>
2016-11-16 00:14:26 -0800

    EZ Hero Button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,7 @@
       $(function() {
-        //EZ Typed.js
-        $("#typed").typed({
-            strings: ["sharing", "imagining","learning"],
-            // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
-            stringsElement: null,
-            // typing speed
-            typeSpeed: 0,
-            // time before typing starts
-            startDelay: 0,
-            // backspacing speed
-            backSpeed: 0,
-            // shuffle the strings
-            shuffle: false,
-            // time before backspacing
-            backDelay: 500,
-            // loop
-            loop: true,
-            // false = infinite
-            loopCount: false,
-            // show cursor
-            showCursor: true,
-            // character for cursor
-            cursorChar: "",
-            // attribute to type (null == text)
-            attr: null,
-            // either html or text
-            contentType: 'html',
-            // call when done callback function
-            callback: function() {},
-            // starting callback function before each string
-            preStringTyped: function() {},
-            //callback for every typed string
-            onStringTyped: function() {},
-            // callback for reset
-            resetCallback: function() {}
+        //EZ Smooth Scroll`
+        $('a[href*=#]').on('click', function(e) {
+            e.preventDefault();
+            $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
         });
       });
