afbe96ea3cb269861143db1ae8dafefbd97c038e
isaacaddis <isaacaddis21@gmail.com>
2016-11-20 16:23:01 -0800

    Major Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,6 @@
       $(function() {
-        //EZ Smooth Scroll`
-        $('a[href*=#]').on('click', function(e) {
-            e.preventDefault();
-            $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+        $(".button-collapse").sideNav();
+        $( "a.email" ).hover(function() {
+        	$(this).addClass( "animated bounceInRight" );
         });
       });
