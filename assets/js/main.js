a5d12679939b26c01a70e3e66f69bea216279bff
Isaac Addis <isaacaddis21@gmail.com>
2017-11-21 18:07:53 -0800

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,66 +1,9 @@
-  $(document).ready(function() {
+  function displaySpeakerApplication(that){
+      if(that.value=="speaker")
+      {
+            $('#getInvolvedForm').html("<a class = \"btn red darken-1\">Go to our Speaker Application Form</a>")
+      }
+}
+  $(document).ready(function(){
     $('select').material_select();
   });
-      // $(function() {
-      //   $(".button-collapse").sideNav();
-      //   $( "a.email" ).hover(function() {
-      //   	$(this).addClass( "animated bounceInRight" );
-      //   });
-      //   // Create a Stripe client
-      //   var stripe = Stripe('sk_test_8M4LiC91VUzF2BT31CEhSGWk');
-
-      //   // Create an instance of Elements
-      //   var elements = stripe.elements();
-
-      //   // Custom styling can be passed to options when creating an Element.
-      //   // (Note that this demo uses a wider set of styles than the guide below.)
-      //   var style = {
-      //     base: {
-      //       color: '#32325d',
-      //       lineHeight: '24px',
-      //       fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-      //       fontSmoothing: 'antialiased',
-      //       fontSize: '16px',
-      //       '::placeholder': {
-      //         color: '#aab7c4'
-      //       }
-      //     },
-      //     invalid: {
-      //       color: '#fa755a',
-      //       iconColor: '#fa755a'
-      //     }
-      //   };
-
-      //   // Create an instance of the card Element
-      //   var card = elements.create('card', {style: style});
-
-      //   // Add an instance of the card Element into the `card-element` <div>
-      //   card.mount('#card-element');
-
-      //   // Handle real-time validation errors from the card Element.
-      //   card.addEventListener('change', function(event) {
-      //     var displayError = document.getElementById('card-errors');
-      //     if (event.error) {
-      //       displayError.textContent = event.error.message;
-      //     } else {
-      //       displayError.textContent = '';
-      //     }
-      //   });
-
-      //   // Handle form submission
-      //   var form = document.getElementById('payment-form');
-      //   form.addEventListener('submit', function(event) {
-      //     event.preventDefault();
-
-      //     stripe.createToken(card).then(function(result) {
-      //       if (result.error) {
-      //         // Inform the user if there was an error
-      //         var errorElement = document.getElementById('card-errors');
-      //         errorElement.textContent = result.error.message;
-      //       } else {
-      //         // Send the token to your server
-      //         stripeTokenHandler(result.token);
-      //       }
-      //     });
-      //   });
-      // });
--- contact.php
+++ contact.php
@@ -1,3 +1,8 @@
 <?php
-	
+	if(isset($_POST['submit'])){
+		mail("isaacaddis21@gmail.com","TEDx Inquiry From"+$_POST['email'],$_POST['name']+" would like to be a "+$_POST['role']+". Email back at: "+$_POST['email']);
+	}
+	if(isset($_POST['action'])){
+		mail("isaacaddis21@gmail.com","TEDx Inquiry From"+$_POST['mail'],$POST['text']);
+	}
 ?>
