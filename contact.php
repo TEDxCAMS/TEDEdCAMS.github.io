d19592848a488b8eef0ceded8c4fa810fea3154d
Isaac Addis <isaacaddis21@gmail.com>
2017-11-22 21:54:57 -0800

    Update contact.php to redirect back after submit

--- contact.php
+++ contact.php
@@ -1,8 +1,10 @@
 <?php
 	if(isset($_POST['submit'])){
 		mail("isaacaddis21@gmail.com","TEDx Inquiry From"+$_POST['email'],$_POST['name']+" would like to be a "+$_POST['role']+". Email back at: "+$_POST['email']);
+		header("Location: index.html");
 	}
 	if(isset($_POST['action'])){
 		mail("isaacaddis21@gmail.com","TEDx Inquiry From"+$_POST['mail'],$POST['text']);
+		header("Location: index.html");
 	}
 ?>
