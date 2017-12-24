b37c759f36a26977b009274e5e33762a0723dcf2
Isaac Addis <isaacaddis21@gmail.com>
2017-12-23 21:49:41 -0800

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,11 @@
   function displaySpeakerApplication(that){
       if(that.value=="speaker")
       {
-            $('#getInvolvedForm').html("<a class = \"btn red darken-1\">Go to our Speaker Application Form</a>")
+            $('#getInvolvedForm').html("<a href = \"https://docs.google.com/forms/d/e/1FAIpQLSe-FSnLZLYQF9uiXdp6i6T282zeFRxtGnB-3-r3QuYCRWziRg/viewform\" class = \"btn red darken-1\">Fill out our interest form!</a>")
       }
 }
   $(document).ready(function(){
     $('select').material_select();
+    var scroll = new SmoothScroll('a[href*="#"]');
+    $(".button-collapse").sideNav();
   });
